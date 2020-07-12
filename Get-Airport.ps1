50d2b2469a6a6c3c9c6874497d6c533dc37de931
noaboa97 <30494947+noaboa97@users.noreply.github.com>
2020-07-12 02:22:57 +0200

    wrapped in a function

--- Get-Airport.ps1
+++ Get-Airport.ps1
@@ -1,15 +1,16 @@
+function Get-Airport{
+  $network = /System/Library/PrivateFrameworks/Apple80211.framework/Resources/airport -I
 
-$network = /System/Library/PrivateFrameworks/Apple80211.framework/Resources/airport -I
+  $state = $network.replace(" ","")
+  $Object = New-Object PSObject
+  foreach ($list in $state){
 
-$state = $network.replace(" ","")
-$Object = New-Object PSObject
-foreach ($list in $state){
+    $name = $list.split(":")[0]
+    $value = $list.split(":")[1]
 
-  $name = $list.split(":")[0]
-  $value = $list.split(":")[1]
+    $Object | add-member Noteproperty $name $value
 
-  $Object | add-member Noteproperty $name $value
+  }
 
+  return $Object
 }
-
-$object
