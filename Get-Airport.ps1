a57392eab5bc3a5f395ce866af00fe9df3389787
noaboa97 <30494947+noaboa97@users.noreply.github.com>
2020-07-12 02:08:33 +0200

    initial
    created the script

---
+++ Get-Airport.ps1
@@ -0,0 +1,15 @@
+
+$network = /System/Library/PrivateFrameworks/Apple80211.framework/Resources/airport -I
+
+$state = $network.replace(" ","")
+$Object = New-Object PSObject
+foreach ($list in $state){
+
+  $name = $list.split(":")[0]
+  $value = $list.split(":")[1]
+
+  $Object | add-member Noteproperty $name $value
+
+}
+
+$object
